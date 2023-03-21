9a5d1234a040a107ec8be05c372373f8d5b5b776
knowOne08 <yashdarji23082004@gmail.com>
2023-03-21 15:00:14 +0530

    Testing\

--- dynamic-places/script.js
+++ dynamic-places/script.js
@@ -1,33 +1,69 @@
 // getting places from APIs
 function loadPlaces(position) {
-    const params = {
-        radius: 300,    // search places not farther than this value (in meters)
-        clientId: '<your-client-id>',
-        clientSecret: '<your-client-secret>',
-        version: '20300101',    // foursquare versioning, required but unuseful for this demo
-    };
-
-    // CORS Proxy to avoid CORS problems
-    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-    // Foursquare API (limit param: number of maximum places to fetch)
-    const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-        &ll=${position.latitude},${position.longitude}
-        &radius=${params.radius}
-        &client_id=${params.clientId}
-        &client_secret=${params.clientSecret}
-        &limit=30 
-        &v=${params.version}`;
-    return fetch(endpoint)
-        .then((res) => {
-            return res.json()
-                .then((resp) => {
-                    return resp.response.venues;
-                })
-        })
-        .catch((err) => {
-            console.error('Error with places API', err);
-        })
+    const method = 'static';
+    // const method = 'api';
+    if(method === 'static') {
+        return [
+            {
+                name: "LIBRARY",
+                location: {
+                    lat: 23.108156404203143, // add here latitude if using static data
+                    lng: 72.59498734978379, // add here longitude if using static data
+                }
+            },
+            {
+                name: "A-block",
+                location: {
+                    lat: 23.1064422997269,  // add here latitude if using static data
+                    lng: 72.59575386356772, // add here longitude if using static data
+                }
+            },
+            {
+                name: "M-Block",
+                location: {
+                    lat: 23.10796558905296, // add here latitude if using static data
+                    lng:  72.59461530062512, // add here longitude if using static data
+                }
+            },
+            {
+                name: "GTU",
+                location: {
+                    lat: 23.105923398933385,  // add here latitude if using static data
+                    lng: 72.59414710034639, // add here longitude if using static data
+                }
+            },
+        ];
+    } else {
+        const params = {
+            radius: 300,    // search places not farther than this value (in meters)
+            clientId: '<your-client-id>',
+            clientSecret: '<your-client-secret>',
+            version: '20300101',    // foursquare versioning, required but unuseful for this demo
+        };
+    
+        // CORS Proxy to avoid CORS problems
+        const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+    
+        // Foursquare API (limit param: number of maximum places to fetch)
+        const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
+            &ll=${position.latitude},${position.longitude}
+            &radius=${params.radius}
+            &client_id=${params.clientId}
+            &client_secret=${params.clientSecret}
+            &limit=30 
+            &v=${params.version}`;
+        return fetch(endpoint)
+            .then((res) => {
+                return res.json()
+                    .then((resp) => {
+                        return resp.response.venues;
+                    })
+            })
+            .catch((err) => {
+                console.error('Error with places API', err);
+            })
+    }
+    
 };
 
 
