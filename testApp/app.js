5bab978354423b71e5ddf182c0fbf979ee873a24
knowOne08 <yashdarji23082004@gmail.com>
2023-03-25 21:06:15 +0530

    Testing

--- testApp/app.js
+++ testApp/app.js
@@ -1,20 +1,17 @@
 // Wait for the scene to load
 document.querySelector('a-scene').addEventListener('loaded', function () {
-    // Get the user's current location
-    navigator.geolocation.watchPosition(function (position) {
-      // Set the GPS camera position
-      var camera = document.querySelector('a-entity[camera]');
-      camera.setAttribute('gps-camera', 'latitude:' + position.coords.latitude + '; longitude:' + position.coords.longitude + ';');
-      
-      // Get the distance to the destination location
-      var destination = document.querySelector('#destination');
-      var distance = destination.getAttribute('location-based').distance;
-      
-      // Check if the user is within a certain distance of the destination
-      if (distance <= 20) {
-        // Display a message or trigger an event
-        console.log('You have reached your destination!');
-      }
-    });
+  // Get the user's current location
+  navigator.geolocation.getCurrentPosition(function (position) {
+    // Set the GPS camera position
+    var camera = document.querySelector('a-entity[camera]');
+    camera.setAttribute('gps-camera', 'latitude:' + position.coords.latitude + '; longitude:' + position.coords.longitude + ';');
+    
+    // Get the destination entity
+    var destination = document.querySelector('#destination');
+    
+    // Set the position of the destination entity based on its GPS coordinates
+    var latitude = destination.getAttribute('gps-entity-place').latitude;
+    var longitude = destination.getAttribute('gps-entity-place').longitude;
+    destination.object3D.position.copy(camera.components['gps-camera'].convertCoordsFromLatLngToVec3(latitude, longitude));
   });
-  
+});
