86198fc9c6b8ae898e97fba3944783dd47e3ff28
knowOne08 <yashdarji23082004@gmail.com>
2023-03-25 20:38:37 +0530

    Testing

--- testApp/index.js
+++ testApp/index.js
@@ -1,15 +1,20 @@
-// Initialize the AR.js scene
-let scene = document.querySelector('a-scene');
-
-// Wait for the scene to load before initializing the markers
-scene.addEventListener('loaded', function () {
-  // Initialize the markers
-  let startMarker = document.querySelector('#start');
-  let endMarker = document.querySelector('#end');
-
-  // Add a click event listener to the end marker
-  endMarker.addEventListener('click', function () {
-    // Redirect the user to the start marker
-    window.location.href = "#start";
+// Wait for the scene to load
+document.querySelector('a-scene').addEventListener('loaded', function () {
+    // Get the user's current location
+    navigator.geolocation.watchPosition(function (position) {
+      // Set the GPS camera position
+      var camera = document.querySelector('a-entity[camera]');
+      camera.setAttribute('gps-camera', 'latitude:' + position.coords.latitude + '; longitude:' + position.coords.longitude + ';');
+      
+      // Get the distance to the destination location
+      var destination = document.querySelector('#destination');
+      var distance = destination.getAttribute('location-based').distance;
+      
+      // Check if the user is within a certain distance of the destination
+      if (distance <= 20) {
+        // Display a message or trigger an event
+        console.log('You have reached your destination!');
+      }
+    });
   });
-});
+  
